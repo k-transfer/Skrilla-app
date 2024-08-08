d4b6cca1d59ab584305c5c413e643652a051a648
Ky Marie <clanphere15@gmail.com>
2024-08-08 11:53:50 +0000

    user router logic inplemented

---
+++ routes/userRoutes.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const { register, login, transfer } = require('../controllers.userController');
+const router = express.Router();
+
+router.post('/register', register);
+router.post('/login', login);
+router.post('/transfer', transfer);
+
+module.exports = router;

f7ce55d7aef538c74eeb4233643abfc445dcbeb5
Ky Marie <clanphere15@gmail.com>
2024-08-08 11:18:09 +0000

    login, register and transfer money data input

---
+++ controllers/userController.js
@@ -0,0 +1,50 @@
+const User = require('../models/user');
+
+// Register a new user
+exports.register = async (req, res) => {
+    try {
+        const { name, email. password } = req.body;
+        const user = new User({ name, email, password });
+        await user.save();
+        res.status(201).json(user);
+    } catch (err) {
+        res.status(400).json({ error: err.message });
+    }
+};
+
+// Login in user logic
+exports.login = async (req, res) => }
+try {
+    const { email, password } = req.body;
+    const user = await User.findOne({ email, password });
+    if (!user) {
+        return res.status(400).json({ error: err.message });
+    }
+};
+
+// Transfer Money
+exports.transfer = async (req, res) => {
+    try {
+        const { fromEmail, toEmail, amount } = req.body;
+        const fromUser = await User.findOne({ email: fromEmail });
+        const toUser = await User.findOne({ email: toEmail });
+
+        if (!fromUser || !toUser) {
+            return res.status(404).json({ error: 'User not found!' });
+        }
+
+        if (fromUser.balance < amount) {
+            return res.status(400).json({ error: 'Insufficient funds!' });
+        }
+
+        fromUser.balance -= amount;
+        toUser.balance += amount;
+
+        await fromUser.save();
+        await toUser.save();
+
+        res.status(200).json({ message: 'Transfer was successful!' });
+    } catch (err) {
+        res.status(500).json({ error: err.message });
+    }
+};
